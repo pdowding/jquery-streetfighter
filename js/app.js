fd0de3f8ec01862c8e259970228f595d71523247
Philip Dowding <phil_dowding@hotmail.com>
2014-04-03 17:35:37 +0100

    Changed animate positions for hadouken

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
@@ -8,16 +8,30 @@ $(document).ready(function(){
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		
-		// play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// animate hadouken to the right of the screen
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '1000px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '412px');
+			}
+		);
 
 	})
-	.mouseup (function() {
+	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken () {
+	$('#hadouken-sound') [0].volume = 0.5;
+	$('#hadouken-sound') [0].load(); 
+	$('#hadouken-sound') [0].play();
+}
+
+
